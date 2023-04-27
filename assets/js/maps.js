20cb15976631369dc189ee7b714c643086f095d1
Lilla-Kavecsanszki <>
2023-04-27 13:21:54 +0000

    Add favicon

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,17 +2,17 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 53.0000,
+            lng: 9.0000
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
+        { lat: 51.51178, lng: -0.123191 },
+        { lat: 51.518005, lng: -0.082705 },
+        { lat: 51.513561, lng: -0.137706 }
     ];
 
     var markers = locations.map(function(location, i) {
